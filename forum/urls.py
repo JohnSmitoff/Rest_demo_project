b9987cf78aa03f5f5eb7c9050c8c984ba372f273
JohnSmitoff <49408501+JohnSmitoff@users.noreply.github.com>
2019-04-23 18:52:58 +0300

    Update urls.py
    updated urls

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,16 @@
-from django.urls import re_path
+from django.urls import path, re_path
 from . import views
 
-
 urlpatterns = [
-    re_path('^questions/$', views.QuestionsList.as_view(), name='questions'),
-    re_path('^questions/(?P<question_id>\d+)/$', views.QuestionDetails.as_view(), name='question-details')
-    re_path('^questions/(?P<question_id>\d+)/answers/(?P<answer_id>\d+)$', views.QuestionDetails.as_view(), name='question-details')
-
+    re_path("^questions/$", views.QuestionList.as_view(), name="questions"),
+    re_path(
+        "^questions/(?P<question_id>\d+)/$",
+        views.QuestionDetails.as_view(),
+        name="question-details",
+    ),
+    re_path(
+        "^questions/(?P<question_id>\d+)/answer/(?P<answer_id>\d+)/$",
+        views.AnswerDetail.as_view(),
+        name="answer-detail"
+    )
 ]
